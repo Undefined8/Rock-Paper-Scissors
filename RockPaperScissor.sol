e54f9a7c1fd15f1119341351f822eeb55c6cfa00
Undefined8 <dzamilatskiy@gmail.com>
2018-07-19 11:07:34 -0400

    filled in the other 6 possible outcomes

--- RockPaperScissor.sol
+++ RockPaperScissor.sol
@@ -11,45 +11,52 @@ The idea is that two players play against eachother.
 */
 
 contract RockPaperScissor{
-    
+
     enum playState {ROCK, PAPER, SCISSOR}
-    
+
     address player1Address = 0x0;
     address player2Address = 0x0;
-    
+
     playState player1;
     playState player2;
-    
-    uint cost = 1000000;
-    
+
+    uint cost = 1000
+
+    // TODO: create function that updates the cost to either 0.01, 0.1 or 1 ETH
+    // reminder: 1 ETH = 10^18 WEI
+    // so cost for 1 ETH should be 1000000000000000000000
+
     function reward(address winner) private {
         winner.send(cost*2);
     }
-    
+
     function tie(address p1, address p2) private {
         p1.send(cost);
         p2.send(cost);
     }
-    
+
     function setPlayers () private {
         if(player1Address == 0x0) {
             player1Address = msg.sender;
-        } else {
+        } else if(player2Address == 0x0){
             player2Address = msg.sender;
         }
     }
-    
+
+    // PUBLIC FUNCTIONS BELOW
+    /////////////////////////
+
     function playRock () public {
       require(msg.value >= cost);
       setPlayers();
       if(msg.sender == player1Address){
-            player1 = playState.ROCK;
-        }
+        player1 = playState.ROCK;
+      }
       if(msg.sender == player2Address){
-            player2 = playState.ROCK;
-        }
+        player2 = playState.ROCK;
+      }
     }
-    
+
     function playPaper () public {
         require(msg.value >= cost);
         setPlayers();
@@ -60,7 +67,7 @@ contract RockPaperScissor{
             player2 = playState.PAPER;
         }
     }
-    
+
     function playScissor () public {
         require(msg.value >= cost);
         setPlayers();
@@ -71,8 +78,8 @@ contract RockPaperScissor{
             player2 = playState.SCISSOR;
         }
     }
-    
-    function determineGameAndReward () public { 
+
+    function determineGameAndReward () public {
         if(player1 == playState.ROCK && player2 == playState.PAPER){
             reward(player1Address);
         }
@@ -82,11 +89,11 @@ contract RockPaperScissor{
         if(player1 == playState.ROCK && player2 == playState.ROCK){
             tie(player1Address, player2Address);
         }
-        
+
         // TODO: fill in the other 6 possible outcomes
-        
+
         player1Address = 0x0;
         player2Address = 0x0;
-        
+
     }
 }
