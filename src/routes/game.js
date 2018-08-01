95b4a4c69be755af9d70a72268ca6459c2a3609e
Undefined8 <dzamilatskiy@gmail.com>
2018-08-01 18:01:19 -0400

    added bare bones game logic (bugs)

--- src/routes/game.js
+++ src/routes/game.js
@@ -7,22 +7,86 @@ import '../App.css'
 
 export default class Game extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerChoice: '',
+      opponentChoice: '',
+      winner: ''
+    }
+  }
+
+  playGame(choice) {
+
+    this.setState({playerChoice: choice});
+    let randomNumber = Math.floor(Math.random() * 3)
+
+    switch (randomNumber){
+      case 0:
+        this.setState({opponentChoice: 'rock'});
+        break;
+      case 1:
+        this.setState({opponentChoice: 'paper'});
+        break;
+      case 2:
+        this.setState({opponentChoice: 'scissors'});
+        break;
+    }
+
+    let player = this.state.playerChoice;
+    let opponent = this.state.opponentChoice;
+
+
+    if(player == 'rock') {
+        if(opponent == 'scissors'){
+            this.setState({winner: 'You'});
+        }else if(opponent == 'paper'){
+            this.setState({winner: 'Not you'});
+        }else{
+            this.setState({winner: 'No one'});
+        }
+    }else if(player == 'paper') {
+        if(opponent == 'rock'){
+            this.setState({winner: 'You'});
+        }else if(opponent == 'scissors'){
+            this.setState({winner: 'Not you'});
+        }else{
+            this.setState({winner: 'No one'});
+        }
+    }else if(player == 'scissors') {
+        if(opponent == 'paper'){
+            this.setState({winner: 'You'});
+        }else if(opponent == 'rock'){
+            this.setState({winner: 'Not you'});
+        }else{
+            this.setState({winner: 'No one'});
+        }
+    }
+  }
+
   render(){
     return (
       <div>
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1 id="header">Pick</h1>
+              <h1 className="center">Pick</h1>
+              <p className="center">Betting: {this.props.location.state.ether} eth</p>
             </div>
             <div className="pure-u-1-3">
-              <button className="pure-button">Rock</button>
+              <button className="pure-button" onClick={() => this.playGame('rock')}>Rock</button>
             </div>
             <div className="pure-u-1-3">
-              <button className="pure-button">Paper</button>
+              <button className="pure-button" onClick={() => this.playGame('paper')}>Paper</button>
             </div>
             <div className="pure-u-1-3">
-              <button className="pure-button">Scissors</button>
+              <button className="pure-button" onClick={() => this.playGame('scissors')}>Scissors</button>
+            </div>
+            <div className="pure-u-1-1">
+              <p>Your choice: {this.state.playerChoice}</p>
+              <p>Opponent choice: {this.state.opponentChoice}</p>
+              <p></p>
+              <p>Winner: {this.state.winner}</p>
             </div>
           </div>
         </main>
--- src/routes/home.js
+++ src/routes/home.js
@@ -9,26 +9,35 @@ import {Link} from 'react-router-dom'
 
 export default class Home extends React.Component {
 
+  routeToGame(eth) {
+    return {
+      pathname: '/game',
+      state: {
+        ether: eth
+      }
+    }
+  }
+
   render(){
     return (
       <div>
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1 id="header">Pick your wager</h1>
+              <h1 className="center">Pick your wager</h1>
             </div>
             <div className="pure-u-1-3">
-              <Link to="/game" className="pure-button">0.01 eth</Link>
+              <Link to={this.routeToGame(0.01)} className="pure-button">0.01 eth</Link>
             </div>
             <div className="pure-u-1-3">
-              <Link to="/game" className="pure-button">0.1 eth</Link>
+              <Link to={this.routeToGame(0.1)} className="pure-button">0.1 eth</Link>
             </div>
             <div className="pure-u-1-3">
-              <Link to="/game" className="pure-button">1 eth</Link>
+              <Link to={this.routeToGame(1)} className="pure-button">1 eth</Link>
             </div>
           </div>
         </main>
-      </div>);
-
+      </div>
+    );
   }
 }
