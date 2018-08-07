9d831eb6fdc2da188777ff5b6220bdeaac05f649
kejace <kejace@gmail.com>
2018-08-07 12:17:46 -0400

    Added RPS

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,14 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var RPS = artifacts.require("./RPS.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy( RPS
+                 , 0x805cdcf68cc12220f89425be89daa44dbc83ff30 
+                 , 0
+                 , 1
+                 , 2
+                 , 2
+                 , 0x805cdcf68cc12220f89425be89daa44dbc83ff30
+                 );
 };
